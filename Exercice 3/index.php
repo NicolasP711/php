3bf4a640787814b6263ccea8896109cf05311d36
Nicolas <nicolaspagani@outlook.fr>
2020-12-09 15:47:39 +0100

    Fin de l'exercice 3

---
+++ Exercice 3/index.php
@@ -0,0 +1,32 @@
+<?php
+$admin = true;
+$colorText = 'red';
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+    b{
+        color: <?php echo $colorText; ?>;
+    }
+
+    </style>
+</head>
+<body>
+    <?php
+
+
+        if($admin = true){
+            echo '<p>Tu es le bienvenu<a href="https://google.com" target="_blank"> va sur Google</a></p>';
+        } else{
+            echo '<b>Vire de là</b>';
+        }
+
+    ?>
+</body>
+</html>
