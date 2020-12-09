24390fd77839590e9eb7d92a99e6676193e02edc
Nicolas <nicolaspagani@outlook.fr>
2020-12-09 14:41:00 +0100

    Correction définitive de l'exercice 2

--- Exercice 2/index.php
+++ Exercice 2/index.php
@@ -1,19 +1,24 @@
+<?php
+
+    $pageBackgroundColor = 'red';
+    $userName ='Alice';
+
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            background-color: <?php echo $pageBackgroundColor; ?>;
+        }
+    </style>
 </head>
 <body>
-    <?php
 
-$color = 'red';
-$name = 'Nicolas';
+    <h1>Bonjour <?php echo $userName; ?> !</h1>
 
-echo 'style ="background-color:red"';
-echo "<h1>Bonjour $name</h1>";
-
-    ?>
 </body>
 </html>
