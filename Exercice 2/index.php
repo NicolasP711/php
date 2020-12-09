ba1ea6e076faf2cec02bb2317aa5e1aa36f58240
Nicolas <nicolaspagani@outlook.fr>
2020-12-09 14:11:57 +0100

    Fin de l'exercice 2

---
+++ Exercice 2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$color = 'red';
+$name = 'Nicolas';
+
+echo '<body style ="background-color:red">';
+echo "<h1>Bonjour $name</h1>";
+
+    ?>
+</body>
+</html>
