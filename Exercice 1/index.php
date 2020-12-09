ff8820a743563b67ead619d86eb2e13f80d13ea9
Nicolas <nicolaspagani@outlook.fr>
2020-12-09 11:45:46 +0100

    fin de l'exercice 1

---
+++ Exercice 1/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Liste à puce</h1>
+    <?php
+    echo "<ul>
+            <li>Fraise</li>
+            <li>Banane</li>
+            <li>Pastèque</li>
+            <li>Cerise</li>
+            <li>Melon</li>
+        </ul>"
+    ?>
+</body>
+</html>
