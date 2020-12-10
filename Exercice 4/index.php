b614a26667a9bb2fda69f13f79d2c17d0e0bf916
Nicolas <nicolaspagani@outlook.fr>
2020-12-10 09:16:04 +0100

    Presque fin de l'exercice 4

---
+++ Exercice 4/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $i = 0;
+
+    while($i <= 5000){
+        echo "<ul>
+                  <li>$i</li>
+              </ul>";
+        $i++;
+    }
+
+
+    ?>
+</body>
+</html>
