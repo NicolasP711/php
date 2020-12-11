2487d25e2155003e1981302589d3966f1a2856cd
Nicolas <nicolaspagani@outlook.fr>
2020-12-11 11:40:36 +0100

    Correction définitive de l'exo 9

--- Exercice 9/index.php
+++ Exercice 9/index.php
@@ -1,6 +1,20 @@
 <?php
 $fruits = ['Pomme', 'Poire', 'Banane', 'Pasteque', 'Figue'];
 
+// Fonction permettant d'afficher le contenu d'une variable avec un print_r entouré d'une balise html <pre>
+function print_rv2($elementToDisplay){
+    echo '<pre>';
+    print_r($elementToDisplay);
+    echo '</pre>';
+}
+
+// Fonction qui retourne un prix initial additionné avec une taxe précisé en second paramètre (20% par défaut)
+function getTTCPrice($ht, $tax = 20){
+    //Version simplifiée : return $ht * (($tax / 100) + 1);
+    $taxToAdd = $ht * $tax / 100;
+    $finalPrice = $ht + $taxToAdd;
+    return $finalPrice;
+}
 
 
 ?>
@@ -13,10 +27,13 @@ $fruits = ['Pomme', 'Poire', 'Banane', 'Pasteque', 'Figue'];
 </head>
 <body>
     <?php
-    function print_rv2($elementToDisplay){
-        return '<pre>' . $elementToDisplay . '</pre>';
-    }
     echo print_rv2('Bonjour');
+    // Affichage de l'array des fruits avec la nouvelle fonction
+    echo print_rv2($fruits);
+
+    // Affichage du prix TTC de 19€
+    echo getTTCprice(19);
+
     ?>
 </body>
 </html>
