86776e10969f5d7172a9d304805df1cd1a31c414
Nicolas <nicolaspagani@outlook.fr>
2020-12-16 10:12:14 +0100

    Correction Exo 14

--- Exo 14/index.php
+++ Exo 14/index.php
@@ -1,12 +1,29 @@
 <?php
+
+    // Par défaut on assigne la couleur grise à $newColor pour éviter d'avoir une couleur vide dans les cas où il n'y a pas de formulaire ou de cookie.
+    $newColor = 'grey';
+
+    // Si le cookie de sauvegarde de la couleur existe, $newColor prendra la couleur stockée dedans
+    if(isset($_COOKIE['backgroundColor'])){
+        $newColor = $_COOKIE['backgroundColor'];
+    }
+
+    // Appel des variables
     if(isset($_POST['color'])){
-        $cookieColor = $_POST['color'];
-        setcookie('colorCookie', $cookieColor, time() + 24 * 3600, null, null, false, true);
+
+        // Bloc des verifs
         if(mb_strlen($_POST['color']) < 2 || mb_strlen($_POST['color']) > 10){
-            echo 'Mauvais code couleur !';
+            $error = 'Vous devez remplir le champ de couleur';
         }
+
+        // Si pas d'erreur
         if(!isset($error)){
-            echo '<body style="background-color:' . htmlspecialchars($cookieColor) . ';"></body>';
+
+            // $newColor contiendra la couleur qui est envoyée par le formulaire
+            $newColor = $_POST['color'];
+
+            // On crée un nouveau cookie avec la nouvelle couleur
+            setcookie('backgroundColor', $_POST['color'], time() + 24 * 3600,null, null, false, true);
         }
     }
 
@@ -20,10 +37,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    <form action="" method="POST">
-    <input type="text" name="color">
+<body style="background-color:<?php echo htmlspecialchars($newColor); ?>;">
+    <form action="index.php" method="POST">
+    <input type="color" name="color">
     <input type="submit">
     </form>
+
+    <?php
+
+    // Si le message d'erreur existe, on l'affiche
+    if(isset($color)){
+        echo '<p style="color:red;" </p>';
+    }
+
+    ?>
 </body>
 </html>
