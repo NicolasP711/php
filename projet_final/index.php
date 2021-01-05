6beb3bba1f81c014e49bd9973a7eb5fd74b1bcb9
Nicolas <nicolaspagani@outlook.fr>
2021-01-05 08:52:29 +0100

    projet final corrigé

---
+++ projet_final/index.php
@@ -0,0 +1,32 @@
+<?php
+
+// Inclusion du fichier des fonctions
+require 'core/functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Wikifruit</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+    <?php  include 'core/partials/topmenu.php'; ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-8 offset-md-2 py-5">
+                <h1>Accueil Wikifruit</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, velit? Nostrum qui est asperiores voluptatem? Temporibus, maiores quis obcaecati rerum architecto iste cumque iusto sit? Ducimus iure eum facilis nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione incidunt quidem quos nesciunt quis porro laboriosam esse quae laborum, earum temporibus magni odio. Repudiandae, incidunt. Earum porro velit cumque suscipit.</p>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
